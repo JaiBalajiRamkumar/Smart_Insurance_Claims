10ea276dde682f6353469141ec29a7393d9cf959
JaiBalajiRamkumar <95561172+JaiBalajiRamkumar@users.noreply.github.com>
2025-10-01 00:17:43 -0400

    Update import_model.py

--- setup/import_model.py
+++ setup/import_model.py
@@ -1,8 +1,8 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Import Model 
-# MAGIC * Pretrained model in github is put into dbfs (during one-time setup)
-# MAGIC * Here the dbfs model is put into the model registry for consumption during pipeline ingestion/inferencing
+# MAGIC # Import Model into Unity Catalog
+# MAGIC * The pretrained model was copied to a UC Volume during setup.
+# MAGIC * This notebook imports the model from the Volume into the Model Registry in Unity Catalog.
 
 # COMMAND ----------
 
@@ -16,55 +16,78 @@
 # COMMAND ----------
 
 import mlflow
-client = mlflow.tracking.MlflowClient()
+from mlflow.tracking import MlflowClient
+
+client = MlflowClient()
 host_name = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().get("browserHostName")
 
 def display_experiment_uri(experiment_name):
+    """Displays a clickable link to an MLflow Experiment."""
     if host_name:
-        experiment_id = client.get_experiment_by_name(experiment_name).experiment_id
-        uri = "https://{}/#mlflow/experiments/{}".format(host_name, experiment_id)
-        displayHTML("""<b>Experiment URI:</b> <a href="{}">{}</a>""".format(uri,uri))
-        
+        try:
+            experiment_id = client.get_experiment_by_name(experiment_name).experiment_id
+            uri = f"https://{host_name}/#mlflow/experiments/{experiment_id}"
+            displayHTML(f'<b>Experiment URI:</b> <a href="{uri}">{uri}</a>')
+        except Exception as e:
+            print(f"Could not find experiment '{experiment_name}'. Error: {e}")
+
 def display_registered_model_uri(model_name):
+    """Displays a clickable link to a Registered Model in UC."""
     if host_name:
-        uri = f"https://{host_name}/#mlflow/models/{model_name}"
-        displayHTML("""<b>Registered Model URI:</b> <a href="{}">{}</a>""".format(uri,uri))
+        # For UC models, the link format is slightly different
+        uri = f"https://{host_name}/explore/data/models/{model_name}"
+        displayHTML(f'<b>Registered Model URI:</b> <a href="{uri}">{uri}</a>')
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Model Metadata
+# MAGIC ## Model Metadata (from UC Config)
 
 # COMMAND ----------
 
+# The model name is now a 3-level UC name: <catalog>.<schema>.<model>
 model_name = getParam("damage_severity_model_name")
-if len(model_name)==0: raise Exception("ERROR: model name is required")
-print("model_name:",model_name)
+print(f"Unity Catalog Model Name: {model_name}")
 
-experiment_name = getParam("damage_severity_model_dir")
-if len(experiment_name)==0: raise Exception("ERROR: Destination experiment name is required")
-print("experiment_name:",experiment_name)
+# The experiment where the imported run will be created
+experiment_name = getParam("damage_severity_model_experiment")
+print(f"Experiment Name: {experiment_name}")
 
-input_dir = getParam("model_dir_on_dbfs")
-if len(input_dir)==0: raise Exception("ERROR: Input directory is required")
-print("input_dir:",input_dir)
+# The source directory for the model files, now on a UC Volume
+input_dir = getParam("model_input_dir")
+# Note: mlflow-export-import needs a 'dbfs:/' prefix for volume paths
+if not input_dir.startswith("dbfs:"):
+    input_dir_dbfs = "dbfs:" + input_dir
+print(f"Model Input Directory (on Volume): {input_dir_dbfs}")
 
 # COMMAND ----------
 
-# MAGIC %md 
-# MAGIC ## Import model
+# MAGIC %md
+# MAGIC ## Import Model into Unity Catalog Registry
 
 # COMMAND ----------
 
 from mlflow_export_import.model.import_model import ModelImporter
+import mlflow
+
+# Set the registry URI to 'databricks-uc' to work with Unity Catalog
+mlflow.set_registry_uri('databricks-uc')
 
 importer = ModelImporter(mlflow.tracking.MlflowClient())
-importer.import_model(model_name, input_dir, experiment_name, delete_model=True)
+
+# Import the model from the Volume path into the UC model registry
+importer.import_model(
+    model_name=model_name,
+    input_dir=input_dir_dbfs,
+    experiment_name=experiment_name,
+    delete_model=True, # Deletes and overwrites the UC model if it already exists
+    verbose=True
+)
 
 # COMMAND ----------
 
-# MAGIC %md 
-# MAGIC ## Display Model in MLflow 
+# MAGIC %md
+# MAGIC ## Display Model in MLflow
 
 # COMMAND ----------
 
